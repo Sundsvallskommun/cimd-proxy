68a98b1e20d712a6bcdb758ea91ef4fb09c6f5a6
Jonas Klingstedt <jonas.klingstedt@gmail.com>
2025-04-28 11:41:51 +0200

    UF-15680 - Certificate validity check
    WIP - refactors SSL handling to use the built-in SSL bundle functionality
    instead of manual plumbing.

--- src/main/java/se/sundsvall/cimdproxy/configuration/CIMDProperties.java
+++ src/main/java/se/sundsvall/cimdproxy/configuration/CIMDProperties.java
@@ -1,11 +1,8 @@
 package se.sundsvall.cimdproxy.configuration;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.validation.annotation.Validated;
-import se.sundsvall.dept44.common.validators.annotation.ValidBase64;
 
 @Validated
 @ConfigurationProperties(prefix = "cimd")
@@ -13,16 +10,4 @@ public record CIMDProperties(
 
 	@DefaultValue("9971") int port,
 
-	@Valid @NotNull SSL ssl,
-
-	@DefaultValue("true") boolean useCimdChecksum) {
-
-	public record SSL(
-		@DefaultValue("false") boolean enabled,
-
-		@ValidBase64(nullable = true) String serverCert,
-		@ValidBase64(nullable = true) String serverKey,
-		String serverKeyPassword,
-		@ValidBase64(nullable = true) String trustedCert) {
-	}
-}
+	@DefaultValue("true") boolean useCimdChecksum) {}
